1d2b04745842def662125bddc338e1e0be95957b
michaelmirhom <micharby73@gmail.com>
2023-04-23 11:49:47 -0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-const init = () => {
+const init = () => { 
+  const form= document.querySelector("form");
+  form .addEventListener("submit",(e)=>{
+   e.preventDefault()
+   const input = document.querySelector("#searchByID")    
+   fetch(`http://localhost:3000/movies/${input.value}`)
+   .then((Response)=>  Response.json()
+  )
+   .then ((data)=>{
+    const title = document.querySelector("section#movieDetails h4")
+    const summary = document.querySelector("section#movieDetails p")
+    title.innerText = data.title;
+    summary.innerText = data.summary
+    form.reset()
+  })
+    })
+ 
+  };
   
-}
+  document.addEventListener("DOMContentLoaded", init);
 
-document.addEventListener('DOMContentLoaded', init);
